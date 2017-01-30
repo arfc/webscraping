dc41a93d23e8daa052431d1ef703ee104d95e92c
Katy Huff <katyhuff@gmail.com>
2017-01-30 14:46:22 -0600

    this adds changes that will make the code a little more readable and bug-free

--- create_SQLite_table.py
+++ create_SQLite_table.py
@@ -13,50 +13,28 @@ c = conn.cursor()
 c.execute('CREATE TABLE {tn} ({nf} {ft} PRIMARY KEY)'\
         .format(tn=table_name, nf=new_field, ft=field_type))
 
-# New columns to be added
-new_column1 = 'Name'
-new_column2 = 'Long'
-new_column3 = 'Lat'
-new_column4 = 'Country'
-new_column5 = 'Institute'
-new_column6 = 'Thermal Capacity'
-new_column7 = 'Electricity Capacity'
-new_column8 = 'Thermal Efficiency'
-new_column9 = 'Capacity Factor'
-new_column10 = 'Type'
-new_column11 = 'Fuel'
-new_column12 = 'Enrichment'
-
 # New column types
-column_type1 = 'TEXT'
-column_type2 = 'REAL'
+text_type  = 'TEXT'
+real_type = 'REAL'
 
+# New columns to be added
+new_columns = [('Name', text_type),
+               ('Long', real_type),
+               ('Lat', real_type),
+               ('Country', text_type),
+               ('Institute', text_type),
+               ('Thermal Capacity', real_type),
+               ('Electricity Capacity', real_type),
+               ('Thermal Efficiency', real_type),
+               ('Capacity Factor', real_type),
+               ('Type', text_type),
+               ('Fuel', text_type),
+               ('Enrichment', real_type)]
 
 # Adding the columns, without row value
-c.execute("ALTER TABLE {tn} ADD COLUMN '{cn}' {ct}"\
-        .format(tn=table_name, cn=new_column1, ct=column_type1))
-c.execute("ALTER TABLE {tn} ADD COLUMN '{cn}' {ct}"\
-        .format(tn=table_name, cn=new_column2, ct=column_type2))
-c.execute("ALTER TABLE {tn} ADD COLUMN '{cn}' {ct}"\
-        .format(tn=table_name, cn=new_column3, ct=column_type2))
-c.execute("ALTER TABLE {tn} ADD COLUMN '{cn}' {ct}"\
-        .format(tn=table_name, cn=new_column4, ct=column_type1))
-c.execute("ALTER TABLE {tn} ADD COLUMN '{cn}' {ct}"\
-        .format(tn=table_name, cn=new_column5, ct=column_type1))
-c.execute("ALTER TABLE {tn} ADD COLUMN '{cn}' {ct}"\
-        .format(tn=table_name, cn=new_column6, ct=column_type2))
-c.execute("ALTER TABLE {tn} ADD COLUMN '{cn}' {ct}"\
-        .format(tn=table_name, cn=new_column7, ct=column_type2))
-c.execute("ALTER TABLE {tn} ADD COLUMN '{cn}' {ct}"\
-        .format(tn=table_name, cn=new_column8, ct=column_type2))
-c.execute("ALTER TABLE {tn} ADD COLUMN '{cn}' {ct}"\
-        .format(tn=table_name, cn=new_column9, ct=column_type2))
-c.execute("ALTER TABLE {tn} ADD COLUMN '{cn}' {ct}"\
-        .format(tn=table_name, cn=new_column10, ct=column_type1))
-c.execute("ALTER TABLE {tn} ADD COLUMN '{cn}' {ct}"\
-        .format(tn=table_name, cn=new_column11, ct=column_type1))
-c.execute("ALTER TABLE {tn} ADD COLUMN '{cn}' {ct}"\
-        .format(tn=table_name, cn=new_column12, ct=column_type1))
+for col in new_columns: 
+    c.execute("ALTER TABLE {tn} ADD COLUMN '{cn}' {ct}"\
+              .format(tn=table_name, cn=col[0], ct=col[1]))
 
 # Committing changes and closing the connection to the database file
 conn.commit()
